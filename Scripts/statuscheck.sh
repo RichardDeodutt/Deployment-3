bca88838ebb3a1cba736690af437538d1cf05de1
Richard Deodutt <richarddeodutt@gmail.com>
2022-10-09 21:48:25 -0400

    fixed status check

--- Scripts/statuscheck.sh
+++ Scripts/statuscheck.sh
@@ -21,10 +21,8 @@ main(){
     #Install Screenfetch if not already
     aptinstalllog "screenfetch"
     #Log Java Status
-    log "$(echo "Java Version")"
-    java -version > /dev/null 2>&1 && log "$(echo ; java -version)" || logwarning "Can't Check the version of Java"
+    java -version > /dev/null 2>&1 && log "$(echo "Java Version"; java -version)" || logwarning "Can't Check the version of Java"
     #Log Jenkins Status
-    log "$(echo "Jenkins Status")"
     systemctl status jenkins --no-pager > /dev/null 2>&1 && log "$(echo "Jenkins Status" ; systemctl status jenkins --no-pager)" || logwarning "Can't Check the Status of Jenkins"
     #Log Jenkins Secret Password if it exists(May not if jenkins is set up already and created a user on the webpage)
     log "$(echo "Secret Password")"
